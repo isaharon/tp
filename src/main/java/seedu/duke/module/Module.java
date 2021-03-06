b165f7f876b2fc3c7d708f4fde7ef6c89aa922f0
isaharon <isabharon@gmail.com>
2021-03-06 18:46:25 +0800

    Update Module.java and ModuleList.java

--- src/main/java/seedu/duke/module/Module.java
+++ src/main/java/seedu/duke/module/Module.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class Module {
 
     private final String moduleCode;
-    public final ArrayList<Lesson> lessonList = new ArrayList<>();
-    public final ArrayList<Task> taskList = new ArrayList<>();
+    private final ArrayList<Lesson> lessonList = new ArrayList<>();
+    private final ArrayList<Task> taskList = new ArrayList<>();
 
     public Module(String moduleCode) {
         this.moduleCode = moduleCode;
--- src/main/java/seedu/duke/module/ModuleList.java
+++ src/main/java/seedu/duke/module/ModuleList.java
@@ -3,21 +3,46 @@ package seedu.duke.module;
 import seedu.duke.storage.Storage;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ModuleList {
 
-    private static final Storage storage = new Storage();
-
-    public static ArrayList<String> moduleList;
+    private ArrayList<String> modules;
     public static Module selectedModule;
 
-    public static boolean setSelectedModule(String name) {
-        if (!moduleList.contains(name)) {
-            //unable to find
-            return false;
+    public ModuleList() {
+        this.modules = new ArrayList<>();
+    }
+
+    public ArrayList<String> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<String> modules) {
+        this.modules = modules;
+    }
+
+    public String addModule(String moduleCode) {
+        modules.add(moduleCode);
+        return moduleCode;
+    }
+
+    public ArrayList<String> deleteModules(ArrayList<Integer> moduleNumbers) {
+        ArrayList<String> deletedModules = new ArrayList<>();
+        Collections.reverse(moduleNumbers);
+        for (Integer moduleNumber : moduleNumbers) {
+            int indexToRemove = moduleNumber - 1;
+            deletedModules.add(0, modules.remove(indexToRemove));
         }
+        return deletedModules;
+    }
+
+    public static Module getSelectedModule() {
+        return selectedModule;
+    }
+
+    public static void setSelectedModule(String name) {
         //selectedModule = storage.loadModule(name);
-        return true;
     }
 
     public static void reset() {
