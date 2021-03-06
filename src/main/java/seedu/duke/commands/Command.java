cf60fb667aec9333cff548db9fbe01a92058cd02
isaharon <isabharon@gmail.com>
2021-03-06 18:45:21 +0800

    Update Command.java methods

--- src/main/java/seedu/duke/commands/Command.java
+++ src/main/java/seedu/duke/commands/Command.java
@@ -1,7 +1,13 @@
 package seedu.duke.commands;
 
+import seedu.duke.exceptions.DukeException;
+import seedu.duke.exceptions.DuplicateModuleCodeException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.Ui;
+
 public abstract class Command {
 
-    public abstract Boolean execute();
+    public abstract void execute(ModuleList modules, Ui ui) throws DukeException;
+    public abstract boolean isExit();
 
 }
