5f728fcad778bd9445126f3dd023ce65b7526c9d
isaharon <isabharon@gmail.com>
2021-03-06 18:50:32 +0800

    Implement AddModuleCommand

---
+++ src/main/java/seedu/duke/commands/AddModuleCommand.java
@@ -0,0 +1,37 @@
+package seedu.duke.commands;
+
+import seedu.duke.exceptions.DuplicateModuleCodeException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.Ui;
+
+import static java.util.Objects.requireNonNull;
+
+public class AddModuleCommand extends Command {
+
+    public static final String MESSAGE_SUCCESS = "Added %s to the module list.";
+    public static final String MESSAGE_DUPLICATE = "Module code %s already exists in the module list";
+
+    private final String moduleCode;
+
+    public AddModuleCommand(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    @Override
+    public void execute(ModuleList modules, Ui ui) throws DuplicateModuleCodeException {
+        requireNonNull(modules);
+
+        for (String moduleCode : modules.getModules()) {
+            if (moduleCode.equals(this.moduleCode)) {
+                throw new DuplicateModuleCodeException(String.format(MESSAGE_DUPLICATE, moduleCode));
+            }
+        }
+        modules.addModule(moduleCode);
+        System.out.println(String.format(MESSAGE_SUCCESS, moduleCode));
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
---
+++ src/test/java/seedu/duke/commands/AddModuleCommandTest.java
@@ -0,0 +1,53 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.DukeException;
+import seedu.duke.exceptions.DuplicateModuleCodeException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.Ui;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddModuleCommandTest {
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Test
+    void execute_uniqueModuleCode_expectPrintSuccess() throws DukeException {
+        System.setOut(new PrintStream(outContent));
+
+        ModuleList moduleList = new ModuleList();
+        moduleList.addModule("CS2105");
+        moduleList.addModule("CS2106");
+
+        String moduleCode = "CS2113T";
+        Command command = new AddModuleCommand(moduleCode);
+        command.execute(moduleList, new Ui());
+
+        String output = String.format(AddModuleCommand.MESSAGE_SUCCESS, moduleCode);
+        assertEquals(output + System.lineSeparator(), outContent.toString());
+
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void execute_duplicateModuleCode_expectDuplicateModuleException() {
+        ModuleList moduleList = new ModuleList();
+        moduleList.addModule("CS2113T");
+
+        String moduleCode = "CS2113T";
+        AddModuleCommand command = new AddModuleCommand(moduleCode);
+        assertThrows(DuplicateModuleCodeException.class, () -> command.execute(moduleList, new Ui()));
+
+    }
+
+    @Test
+    void isExit_singleInput_expectReturnFalse() {
+        assertEquals(false, new AddModuleCommand("CS2113T").isExit());
+    }
+}
