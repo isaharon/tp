cb9465463cb50b0c692d94545b579454e7a0af8d
isaharon <isabharon@gmail.com>
2021-03-06 18:54:20 +0800

    Add exceptions

---
+++ src/main/java/seedu/duke/exceptions/InvalidModuleCodeException.java
@@ -0,0 +1,8 @@
+package seedu.duke.exceptions;
+
+public class InvalidModuleCodeException extends DukeException {
+
+    public InvalidModuleCodeException(String message) {
+        super(message);
+    }
+}
