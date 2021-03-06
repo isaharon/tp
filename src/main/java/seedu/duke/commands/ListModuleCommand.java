0a0c5c50ff64153c8d0a06c83488a5293e1a7dd6
isaharon <isabharon@gmail.com>
2021-03-06 18:53:00 +0800

    Implement ListModuleCommand

---
+++ src/main/java/seedu/duke/commands/ListModuleCommand.java
@@ -0,0 +1,29 @@
+package seedu.duke.commands;
+
+import seedu.duke.common.Messages;
+import seedu.duke.exceptions.DukeException;
+import seedu.duke.module.ModuleList;
+import seedu.duke.ui.Ui;
+
+import static java.util.Objects.requireNonNull;
+
+public class ListModuleCommand extends Command {
+    public static final String MESSAGE_LIST = "Modules in your list:";
+
+    @Override
+    public void execute(ModuleList modules, Ui ui) throws DukeException {
+        requireNonNull(modules);
+        String listMessage = MESSAGE_LIST;
+        for (String module : modules.getModules()) {
+            int moduleNumber = modules.getModules().indexOf(module);
+            listMessage += "\n";
+            listMessage += String.format(Messages.MESSAGE_LIST_ITEMS, moduleNumber + 1, module);
+        }
+        ui.printMessage(listMessage);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
